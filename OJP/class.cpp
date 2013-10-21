2d97aac1f3c11bd8def712c30646755fc2805415
hasiek <hasiek01@gmail.com>
2013-10-21 19:30:45 +0200

    Static elements added.

--- OJP/class.cpp
+++ OJP/class.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 class Man {
 private:
+	static bool is_a_mammal;
+	const string pesel;
 	string name;
 	int age;
 	string marital_status;
@@ -12,23 +14,27 @@ private:
 public:
 	Man();
 	// Man(string name_to_set, int age_to_set, string status, string hometown_to_set);
-	Man(string name_to_set, int age_to_set, string status = "N/A", string hometown_to_set = "N/A");
+	Man(string pesel_to_set, string name_to_set, int age_to_set, string status = "N/A", string hometown_to_set = "N/A");
 	
-	string get_name();
-	void set_name(string name_to_set);
+	string get_pesel() const;
 	
-	int get_age();
-	void set_age(int age_to_set);
+	string get_name() const;
+	void set_name(const string name_to_set);
 	
-	string get_status();
-	void set_status(string status);
+	int get_age() const;
+	void set_age(const int age_to_set);
 	
-	string get_hometown();
-	void set_hometown(string hometown_to_set);
+	string get_status() const;
+	void set_status(const string status);
 	
-	bool is_adult();
-	void birthday(int years = 1);
-	int will_retire_in();
+	string get_hometown() const;
+	void set_hometown(const string hometown_to_set);
+	
+	static bool get_is_a_mammal();
+	
+	bool is_adult() const;
+	void birthday(const int years = 1);
+	int will_retire_in() const;
 	
 };
 
@@ -40,23 +46,23 @@ private:
 	Man promoter;
 public:
 	Student();
-	Student(string name_to_set, int age_to_set, string status, string hometown_to_set, string uni, int year, bool if_public, string promoter_name, int promoter_age);
+	Student(string pesel_to_set, string name_to_set, int age_to_set, string status, string hometown_to_set, string uni, int year, bool if_public, string promoter_pesel, string promoter_name, int promoter_age);
 	
-	string get_university();
-	void set_university(string uni);
+	string get_university() const;
+	void set_university(const string uni);
 	
-	int get_year();
-	void set_year(int year);
+	int get_year() const;
+	void set_year(const int& year);
 	
-	bool get_public();
-	void set_public(bool if_public);
+	bool get_public() const;
+	void set_public(const bool if_public);
 	
-	Man get_promoter();
-	void set_promoter(string name_to_set, int age_to_set);
+	Man get_promoter() const;
+	void set_promoter(const string pesel_to_set, const string name_to_set, const int age_to_set);
 	
-	int finishing_year(); // function returns in how many years student will graduate
-	double students_discount(int ticket_price);
-	string is_working(bool is_he);
+	int finishing_year() const; // function returns in how many years student will graduate
+	double students_discount(int& ticket_price) const;
+	string is_working(const bool is_he) const;
 	
 };
 
@@ -66,181 +72,189 @@ private:
 	int grandchildren;
 public:
 	Pensioner();
-	Pensioner(string name_to_set, int age_to_set, string status, string hometown_to_set, int pension_to_set, int how_many_grandchildren);
+	Pensioner(string pesel_to_set, string name_to_set, int age_to_set, string status, string hometown_to_set, int pension_to_set, int how_many_grandchildren);
 		
-	int get_pension();
-	void set_pension(int pension_to_set);
+	int get_pension() const;
+	void set_pension(const int pension_to_set);
 	
-	int get_grandchildren();
-	void set_grandchildren(int how_many_grandchildren);
+	int get_grandchildren() const;
+	void set_grandchildren(const int how_many_grandchildren);
 	
-	bool if_conjugal_discount(string marital_status);
-	double count_discount(bool if_discount, int pension);
-	double raise_pension(int pension);
+	bool if_conjugal_discount(const string marital_status) const;
+	double count_discount(const bool if_discount, int& pension) const;
+	double raise_pension(int& pension);
 	
 };
 
-Man::Man() {
+bool Man::is_a_mammal = true;
+
+Man::Man():
+	pesel("N/A"),
+	name("John Doe"),
+	age(30),
+	marital_status("N/A"),
+	hometown("N/A"){};
+
+Man::Man(string pesel_to_set, string name_to_set, int age_to_set, string status, string hometown_to_set):
+	pesel(pesel_to_set),
+	name(name_to_set),
+	age(age_to_set),
+	marital_status(status),
+	hometown(hometown_to_set){};
 	
-	name = "John Doe";
-	age = 30;
-	marital_status = "N/A";
-	hometown = "N/A";
+string Man::get_pesel() const {
+
+	return pesel;
 	
 }
 
-Man::Man(string name_to_set, int age_to_set, string status, string hometown_to_set) {
-	
-	name = name_to_set;
-	age = age_to_set;
-	marital_status = status;
-	hometown = hometown_to_set;
-	
-}
-
-string Man::get_name() {
+string Man::get_name() const {
 	
 	return name;
 	
 }
 
-void Man::set_name(string name_to_set) {
+void Man::set_name(const string name_to_set) {
 	
 	name = name_to_set;
 	
 }
 
-int Man::get_age() {
+int Man::get_age() const {
 	
 	return age;
 	
 }
 
-void Man::set_age(int age_to_set) {
+void Man::set_age(const int age_to_set) {
 	
 	age = age_to_set;
 	
 }
 
-string Man::get_status() {
+string Man::get_status() const {
 	
 	return marital_status;
 	
 }
 
-void Man::set_status(string status) {
+void Man::set_status(const string status) {
 	
 	marital_status = status;
 	
 }
 
-string Man::get_hometown() {
+string Man::get_hometown() const {
 	
 	return hometown;
 	
 }
 
-void Man::set_hometown(string hometown_to_set) {
+void Man::set_hometown(const string hometown_to_set) {
 	
 	hometown = hometown_to_set;
 	
 }
 
-bool Man::is_adult() {
+bool Man::get_is_a_mammal() {
+	
+	return is_a_mammal;
+	
+}
+
+bool Man::is_adult() const {
 	
 	return age >= 18;
 }
 
-void Man::birthday(int years) {
+void Man::birthday(const int years) {
 	
 	age += years;
 	
 }
 
-int Man::will_retire_in() {
+int Man::will_retire_in() const {
 	
 	return 65 - age;
 	
 }
 
-Student::Student() {
-	
-	university = "N/A";
-	year_of_studies = 0;
-	if_public_school = true;
-	promoter = Man();
-	
-}
+Student::Student():
+	Man(),
+	university("N/A"),
+	year_of_studies(0),
+	if_public_school(true),
+	promoter(){};
 
-Student::Student(string name_to_set, int age_to_set, string status, string hometown_to_set, string uni, int year, bool if_public, string promoter_name, int promoter_age):
-		Man(name_to_set, age_to_set, status, hometown_to_set),
+Student::Student(string pesel_to_set, string name_to_set, int age_to_set, string status, string hometown_to_set, string uni, int year, bool if_public, string promoter_pesel, string promoter_name, int promoter_age):
+		Man(pesel_to_set, name_to_set, age_to_set, status, hometown_to_set),
 		university(uni),
 		year_of_studies(year),
 		if_public_school(if_public),
-		promoter(promoter_name, promoter_age){};
+		promoter(promoter_pesel, promoter_name, promoter_age){};
 
-string Student::get_university() {
+string Student::get_university() const {
 	
 	return university;
 	
 }
 
-void Student::set_university(string uni) {
+void Student::set_university(const string uni) {
 	
 	university = uni;
 	
 }
 
-int Student::get_year() {
+int Student::get_year() const {
 	
 	return year_of_studies;
 	
 }
 
-void Student::set_year(int year) {
+void Student::set_year(const int& year) {
 	
 	year_of_studies = year;
 	
 }
 
-bool Student::get_public() {
+bool Student::get_public() const {
 	
 	return if_public_school;
 	
 }
 
-void Student::set_public(bool if_public) {
+void Student::set_public(const bool if_public) {
 	
 	if_public_school = if_public;
 	
 }
 
-Man Student::get_promoter() {
+Man Student::get_promoter() const {
 		
 		return promoter;
 		
 }
 
-void Student::set_promoter(string name_to_set, int age_to_set) {
+void Student::set_promoter(const string pesel_to_set, const string name_to_set, const int age_to_set) {
 	
 	promoter.set_name(name_to_set);
 	promoter.set_age(age_to_set);
 	
 }
 
-int Student::finishing_year() {
+int Student::finishing_year() const {
 	
 	return 5 - year_of_studies;
 	
 }
 
-double Student::students_discount(int ticket_price) {
+double Student::students_discount(int& ticket_price) const {
 	
 	return ticket_price * 0.5;
 	
 }
 
-string Student::is_working(bool is_he) {
+string Student::is_working(const bool is_he) const {
 	
 	if (is_he == true) 
 		return "Working student";
@@ -249,53 +263,47 @@ string Student::is_working(bool is_he) {
 
 }
 
-Pensioner::Pensioner() {
-	
-	Man();
-	pension = 0;
-	grandchildren = 0;
-	
-}
+Pensioner::Pensioner():
+	Man(),
+	pension(0),
+	grandchildren(0){};
 
-Pensioner::Pensioner(string name_to_set, int age_to_set, string status, string hometown_to_set, int pension_to_set, int how_many_grandchildren):
-		Man(name_to_set, age_to_set, status, hometown_to_set),
+Pensioner::Pensioner(string pesel_to_set, string name_to_set, int age_to_set, string status, string hometown_to_set, int pension_to_set, int how_many_grandchildren):
+		Man(pesel_to_set, name_to_set, age_to_set, status, hometown_to_set),
 		pension(pension_to_set),
 		grandchildren(how_many_grandchildren){};
 
-int Pensioner::get_pension() {
+int Pensioner::get_pension() const {
 	
 	return pension;
 	
 }
 
-void Pensioner::set_pension(int pension_to_set) {
+void Pensioner::set_pension(const int pension_to_set) {
 	
 	pension = pension_to_set;
 	
 }
 
-int Pensioner::get_grandchildren() {
+int Pensioner::get_grandchildren() const {
 	
 	return grandchildren;
 	
 }
 
-void Pensioner::set_grandchildren(int how_many_grandchildren) {
+void Pensioner::set_grandchildren(const int how_many_grandchildren) {
 	
 	grandchildren = how_many_grandchildren;
 	
 }
 
-bool Pensioner::if_conjugal_discount(string marital_status) {
+bool Pensioner::if_conjugal_discount(const string marital_status) const {
 	
-	if (marital_status == "married")
-		return true;
-	else
-		return false;
+	return marital_status == "married";
 		
 }
 
-double Pensioner::count_discount(bool if_discount, int pension) {
+double Pensioner::count_discount(const bool if_discount, int& pension) const {
 	
 	if (if_discount == true)
 		return pension * 0.15;
@@ -304,7 +312,7 @@ double Pensioner::count_discount(bool if_discount, int pension) {
 		
 }
 
-double Pensioner::raise_pension(int pension) {
+double Pensioner::raise_pension(int& pension) {
 	
 	return pension += pension * 0.1;
 	
@@ -312,18 +320,24 @@ double Pensioner::raise_pension(int pension) {
 
 int main() {
 	
-	Man hairdresser = Man("Mary", 30, "married", "New York");
-	Student lawyer2b = Student("John", 20, "single", "Cambridge", "Cambridge University", 3, true, "Mark", 55);
-	Pensioner johns_granny("Else", 72, "widow", "Yorkshire", 2500, 4);
+	Man hairdresser = Man("73013010295", "Mary", 30, "married", "New York");
+	Student lawyer2b = Student("93020211964", "John", 20, "single", "Cambridge", "Cambridge University", 3, true, "58041267987", "Mark", 55);
+	Pensioner johns_granny("31121267854", "Else", 72, "widow", "Yorkshire", 2500, 4);
 	Man unknown = Man();
+	const Man android = Man();
 	
 	cout << "John's grandmother is " << johns_granny.get_name() << '.' << endl;
 	cout << "John's promoter is " << lawyer2b.get_promoter().get_name() << '.' << endl;
-	hairdresser.birthday();
+	hairdresser.birthday(5);
 	cout << "Mary is " << hairdresser.get_age() << " years old." << endl;
 	cout << "Else lived in " << johns_granny.get_hometown() << '.' << endl;
 	johns_granny.set_hometown("London");
 	cout << "Now Else lives in " << johns_granny.get_hometown() << '.' << endl;
 	cout << "Unknown man is " << unknown.get_name() << '.' << endl;
+	
+	cout << "Android's name is " << android.get_name() << '.' << endl;
+	// android.set_name("Antoni"); - an error occurs while trying to use this function
+	cout << hairdresser.get_is_a_mammal() << endl;
+	
 
 }
